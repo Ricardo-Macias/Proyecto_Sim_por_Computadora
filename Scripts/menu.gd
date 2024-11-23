c7cc829ec06146a02ab6676e3b718e04a5c66994
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2024-11-22 20:35:31 -0600

    feat: crear menu

---
+++ Scripts/menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_jugar_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+
+
+func _on_creditos_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/creditos.tscn")
+
+
+func _on_salir_pressed() -> void:
+	get_tree().quit()
